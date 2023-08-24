62c1323983404e92169ab438bfd8112325fd07f7
Mohammad Ahmed <Mohammad-Ahmed73>
2023-08-25 00:17:08 +0500

    New Changes

--- js/script.js
+++ js/script.js
@@ -25,43 +25,41 @@ jQuery(document).ready(function() {
 	jQuery(window).scroll(function() {    
 		var scroll = jQuery(window).scrollTop();
 		if (scroll > 0) {
-			jQuery(".header-main").addClass("shadow-sm dark-header");
+			jQuery(".header-main").addClass("dark-header");
 		} else {
-			jQuery(".header-main").removeClass("shadow-sm dark-header");
+			jQuery(".header-main").removeClass("dark-header");
 		}
 	});
 });
 
-jQuery('.app-screenshots-slider').slick({
-	dots: true,
-	infinite: false,
+jQuery('.our-partner-slider').slick({
+	dots: false,
+	arrows: false,
+	infinite: true,
 	speed: 300,
-	slidesToShow: 4,
-	slidesToScroll: 4,
-	appendArrows: ".slider-arrows",
-	prevArrow: '<button class="slide-arrow prev-arrow"><i class="fas fa-chevron-left"></i></button>',
-	nextArrow: '<button class="slide-arrow next-arrow"><i class="fas fa-chevron-right"></i></button>',
+	slidesToShow: 5,
+	slidesToScroll: 3,
 	responsive: [
-	{
-		breakpoint: 1024,
-		settings: {
-			slidesToShow: 3,
-			slidesToScroll: 3
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 3
+			}
+		},
+		{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
 		}
-	},
-	{
-		breakpoint: 600,
-		settings: {
-			slidesToShow: 2,
-			slidesToScroll: 2
-		}
-	},
-	{
-		breakpoint: 480,
-		settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1
-		}
-	}
-	]
-});
+		]
+  });
